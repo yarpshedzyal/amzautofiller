1e0ff4a99562ce79d7e0ea5ae0784adc421fb5bf
yarpshe <yar.pshe@gmail.com>
2024-09-10 13:41:39 +0300

    proper selector for price

--- popup.js
+++ popup.js
@@ -23,7 +23,7 @@ function copyOrderInfo() {
   let orderInfo = {
     order_id: document.querySelector('#MYO-app > div > div.a-row.a-spacing-medium > div.a-column.a-span10 > div > div:nth-child(1) > div.a-row.a-spacing-mini > div > span.a-text-bold').innerText,
     asin: document.querySelector('#MYO-app > div > div.a-row.a-spacing-medium > div.a-column.a-span10 > div > div.a-row.a-spacing-large > div > table > tbody > tr > td:nth-child(3) > div > div:nth-child(2) > div > b').innerText,
-    price: document.querySelector('#MYO-app > div > div.a-row.a-spacing-medium > div.a-column.a-span10 > div > div.a-row.a-spacing-large > div > table > tbody > tr > td:nth-child(7) > div > table.a-normal.a-spacing-none > tbody > div:nth-child(3) > div.a-column.a-span6.a-text-right.no-right-margin > span').innerText,
+    price: document.querySelector('#MYO-app > div > div.a-row.a-spacing-medium > div.a-column.a-span10 > div > div.a-row.a-spacing-large > div > table > tbody > tr > td.a-text-right > span').innerText,
     qty: document.querySelector('#MYO-app > div > div.a-row.a-spacing-medium > div.a-column.a-span10 > div > div.a-row.a-spacing-large > div > table > tbody > tr > td:nth-child(5)').innerText,
     phone: document.querySelector('#MYO-app > div > div.a-row.a-spacing-medium > div.a-column.a-span10 > div > div:nth-child(2) > div.a-column.a-span5.a-span-last > div > div > div > div > div:nth-child(2) > div > table > tbody > div > tr > td.a-text-left.a-align-bottom').innerText
   };
@@ -38,11 +38,11 @@ function pasteOrderInfoToSheet() {
     if (orderInfo) {
       // Example of pasting the info to Google Sheets
       // Note: You need to adapt these selectors to match the specific Google Sheets structure
-      document.querySelector('[name="order_id"]').value = orderInfo.order_id;
-      document.querySelector('[name="asin"]').value = orderInfo.asin;
-      document.querySelector('[name="price"]').value = orderInfo.price;
-      document.querySelector('[name="qty"]').value = orderInfo.qty;
-      document.querySelector('[name="phone"]').value = orderInfo.phone;
+      document.querySelector('[name="AMZ ID [order_id]"]').value = orderInfo.order_id;
+      document.querySelector('[name="ASIN [asin]"]').value = orderInfo.asin;
+      document.querySelector('[name="AMZ Order Price [price]"]').value = orderInfo.price;
+      document.querySelector('[name="Quantity [qty]"]').value = orderInfo.qty;
+      document.querySelector('[name="Phone AMZ [phone]"]').value = orderInfo.phone;
     }
   });
 }
